82a9e4492104609268ab9d6ac815ee85c086d1d3
inarmprch <phuricha.rom@gmail.com>
2025-04-06 15:11:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ try:
   model = genai.GenerativeModel('gemini-2.0-flash-lite')
 
   if "chat" not in st.session_state:
-  st.session_state.chat = model.start_chat(history=[])
-  st.title('Gemini Pro Test')
+    st.session_state.chat = model.start_chat(history=[])
+    st.title('Gemini Pro Test')
  
   def role_to_streamlit(role:str) -> str:
     if role == 'model':
@@ -18,11 +18,11 @@ try:
    
   for message in st.session_state.chat.history:
           with st.chat_message(role_to_streamlit(message.role)):
-  st.markdown(message.parts[0].text)
+            st.markdown(message.parts[0].text)
    
       if prompt := st.chat_input("Text Here"):
           st.chat_message('user').markdown(prompt)
-  response = st.session_state.chat.send_message(prompt)
+          response = st.session_state.chat.send_message(prompt)
           with st.chat_message('assistant'):
               st.markdown(response.text)
    
